0ee57a5afc33b392c6c845b78c160cb502269acd
bayrktlihn <bayraktaralihan1995@gmail.com>
2022-08-16 23:52:28 +0300

    prepare project base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { AccountingModule } from './accounting/accounting.module';
+import { IncomeModule } from './income/income.module';
+import { LoginModule } from './login/login.module';
+import { MainModule } from './main/main.module';
+import { MainComponent } from './layouts/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => LoginModule,
+  },
+  {
+    path: '',
+    component: DefaultComponent,
+    loadChildren: () => AccountingModule,
+  },
+  {
+    path: '',
+    component: DefaultComponent,
+    loadChildren: () => IncomeModule,
+  },
+  {
+    path: '',
+    component: MainComponent,
+    loadChildren: () => MainModule,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './layouts/default/default.component';
+import { AccountingModule } from './accounting/accounting.module';
+import { IncomeModule } from './income/income.module';
+import { SharedModule } from './shared/shared.module';
+import { LoginModule } from './login/login.module';
+import { MainComponent } from './layouts/main/main.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DefaultComponent, MainComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MainModule,
+    LoginModule,
+    AccountingModule,
+    IncomeModule,
+    SharedModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
